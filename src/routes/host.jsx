2c8bb8bb62c06e9af8b4930b7b454283a402f6dc
Ikpemosi Arnold <Ikpemosi@protonmail.com>
2023-10-27 02:37:35 +0100

    changed URLS

--- src/routes/host.jsx
+++ src/routes/host.jsx
@@ -4,19 +4,19 @@ import MusicControls from "../components/MusicControls";
 import { io } from "socket.io-client";
 import { Radio } from "react-feather";
 
-const URL = "http://localhost:3000";
+const URL = "https://radio-broadcast-main.vercel.app";
 
 function Host() {
-    const socketRef = useRef(io(URL));
+  const socketRef = useRef(io(URL));
 
-    return (
-        <div className="flex flex-col items-center h-full">
-            <h1 className="text-5xl font-bold">Broadcast</h1>
-            <Radio size={48} className="mb-4" />
-            <MicrophoneButton socket={socketRef.current} />
-            <MusicControls socket={socketRef.current} />
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center h-full">
+      <h1 className="text-5xl font-bold">Broadcast</h1>
+      <Radio size={48} className="mb-4" />
+      <MicrophoneButton socket={socketRef.current} />
+      <MusicControls socket={socketRef.current} />
+    </div>
+  );
 }
 
 export default Host;
